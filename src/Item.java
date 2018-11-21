c43c3bf5f6b909b35fa675304e368f5712dc0e01
Ahmad Mohammad Abdullah <ahmedwahy@gmail.com>
2018-11-21 08:11:23 +0200

    Create Item.java

---
+++ src/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    private String name;
+    private int weight;
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
